872e1adcda483a8b1b384072d3b29a87a67a9cef
Olivier Monnier <omonnier.dev@gmail.com>
2016-06-03 11:42:12 +0200

    use oauth io

--- app/report.js
+++ app/report.js
@@ -1,63 +1,67 @@
-gapi.analytics.ready(function() {
+OAuth.initialize('mr4LvKW1k0jEkIcgDmyQlspDni0')
+OAuth.popup('google_analytics').done(function(result) {
+    // do some stuff with result
+    gapi.analytics.ready(function() {
 
-  /**
-   * Authorize the user immediately if the user has already granted access.
-   * If no access has been created, render an authorize button inside the
-   * element with the ID "embed-api-auth-container".
-   */
-  gapi.analytics.auth.authorize({
-    container: 'embed-api-auth-container',
-    clientid: '78021961985-rtqapis46k3ds07o8c1uv2l29pauk66j.apps.googleusercontent.com'
-  });
+      /**
+       * Authorize the user immediately if the user has already granted access.
+       * If no access has been created, render an authorize button inside the
+       * element with the ID "embed-api-auth-container".
+       */
+      // gapi.analytics.auth.authorize({
+      //   container: 'embed-api-auth-container',
+      //   clientid: '78021961985-rtqapis46k3ds07o8c1uv2l29pauk66j.apps.googleusercontent.com'
+      // });
 
-  gapi.analytics.auth.on('success', function() {
-    $('.view-logged').show();
-  });
+      gapi.analytics.auth.authorize({serverAuth: result});
 
-  var DATA = [];
+      $('.view-logged').show();
 
-  function renderCharts() {
-    var i = 0
-    $('#graph').empty();
-    DATA.forEach(function(stat, index) {
-      i = i + +stat.size;
+      var DATA = [];
 
-      if (i > 12) {
-        i = 0;
-        $('#graph').append('<div class="clearfix"></div>');
+      function renderCharts() {
+        var i = 0
+        $('#graph').empty();
+        DATA.forEach(function(stat, index) {
+          i = i + +stat.size;
+
+          if (i > 12) {
+            i = 0;
+            $('#graph').append('<div class="clearfix"></div>');
+          }
+          viewChartContainer($('#graph'), index, stat);
+        });
       }
-      viewChartContainer($('#graph'), index, stat);
-    });
-  }
 
-  $(document).ready(function() {
-    $(document).on('click', '#formSubmit', function(e) {
-      e.preventDefault();
+      $(document).ready(function() {
+        $(document).on('click', '#formSubmit', function(e) {
+          e.preventDefault();
 
-      DATA.push({
-        type: $('#type').val(),
-        metrics: $('#metrics').val(),
-        dimensions: $('#dimensions').val(),
-        startDate: $('#startDate').val(),
-        endDate: $('#endDate').val(),
-        maxResults: $('#maxResults').val(),
-        sort: $('#sort').val(),
-        filters: $('#filters').val(),
-        segment: $('#segment').val(),
-        size: $('#size').val()
+          DATA.push({
+            type: $('#type').val(),
+            metrics: $('#metrics').val(),
+            dimensions: $('#dimensions').val(),
+            startDate: $('#startDate').val(),
+            endDate: $('#endDate').val(),
+            maxResults: $('#maxResults').val(),
+            sort: $('#sort').val(),
+            filters: $('#filters').val(),
+            segment: $('#segment').val(),
+            size: $('#size').val()
+          });
+
+          renderCharts()
+        });
+
+        $(document).on('click', '.btn-close', function(e) {
+          e.preventDefault();
+
+          var i = $(this).data('view');
+
+          DATA.splice(i, 1);
+
+          renderCharts();
+        });
       });
-
-      renderCharts()
     });
-
-    $(document).on('click', '.btn-close', function(e) {
-      e.preventDefault();
-
-      var i = $(this).data('view');
-
-      DATA.splice(i, 1);
-
-      renderCharts();
-    });
-  });
 });
