9a57732ea524e474663cf3340c3936b15515af1a
Olivier Monnier <omonnier.dev@gmail.com>
2016-06-03 10:23:29 +0200

    heroku build

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var browserSync = require('browser-sync');
 
 browserSync({
-    server: "app",
-    files: ["app/*.html", "app/*.js", "node_modules/**/*"]
+  port: process.env.PORT || 3000,
+  server: "app",
+  files: ["app/*.html", "app/*.js", "node_modules/**/*"]
 });
